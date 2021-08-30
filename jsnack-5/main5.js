fa310a59fba47a3d0fbb4310010b9103f0c8be15
Mikele91 <michele.amoruso91.8@gmail.com>
2021-08-30 10:20:57 +0200

    aggiunta randomicità di inserimento

--- jsnack-5/main5.js
+++ jsnack-5/main5.js
@@ -1,28 +1,42 @@
 // Crea due array che hanno un numero di elementi diversi.
 
-var ele1 = [1, 2, 3, 4, 5];
-var ele2 = [6, 7, 8, 4, 5, 9 , 12, 13, 18];
+// var ele1 = [1, 2, 3, 4, 5];
+// var ele2 = [6, 7, 8, 4, 5, 9 , 12, 13, 18, 1, 3, 4, 55, 20];
+
+var ele1 = [];
+var ele2 =[];
+
+var lunghezzaEle1 =  Math.floor(Math.random() * 50);
+for( var i = 0; i < lunghezzaEle1; i++ ) {
+   ele1.push( Math.floor(Math.random() * 100));
+}
+
+var lunghezzaEle2 =  Math.floor(Math.random() * 50);
+for( var i = 0; i < lunghezzaEle2; i++ ) {
+    ele2.push( Math.floor(Math.random() * 100));
+}
+
 
 console.log(" Grandezza ele1 = " + ele1.length);
 console.log( " Grandezza ele2 = " + ele2.length);
 
-if(ele1.length > ele2.length){
-
-    while (ele1.length != ele2.length){
-           ele2.push(Math.floor(Math.random() *100));
-         
-     }
-}else if(ele1.length < ele2.length) {
-    while (ele1.length != ele2.length){
-        ele1.push(Math.floor(Math.random() *100));
-      
-  }
-}else{
-    console.log("Gli elementi sono uguali");
+if(ele1.length < ele2.length){
+    while( ele1.length != ele2.length){
+        ele1.push( Math.floor(Math.random() * 100));
+        
+    }
+} else if (ele2.length < ele1.length){
+    while( ele1.length != ele2.length){
+        ele2.push( Math.floor(Math.random() * 100));
+        
+    }
 }
 
-console.log(" Grandezza ele1 dopo push = " + ele1.length);
+console.log(" Grandezza ele1 dopo push = " + ele1.length); 
 console.log( " Grandezza ele2 dopo push = " + ele2.length);
+console.log(ele1);
+console.log(ele2);
+
 
 
 // Aggiungi elementi casuali all’array che ha meno elementi,
